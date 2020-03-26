15b554f4ff3c4939ffc3913ee2695ec472c2ad9e
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-03-25 23:34:21 -0400

    add navbar & set map width/height

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react"
 import ReactMapGL, { Popup } from "react-map-gl"
+import { Navbar, Button } from "react-bootstrap"
+import { FaGithub } from "react-icons/fa"
+import useWindowDimensions from "../utils/useWindowDimensions"
 import Marker from "../components/marker"
 import InfoBox from "../components/InfoBox"
 
@@ -7,10 +10,10 @@ const mapStyle = "mapbox://styles/milomilo86/ck83uyrnc2n4l1imsjgjwmghu"
 
 const MapPage = () => {
   const { GATSBY_MABOX_API_ACCESS_TOKEN } = process.env
+
+  const { width, height } = useWindowDimensions()
   const [location, setLocation] = useState(null)
   const [viewport, setViewport] = useState({
-    width: "100%",
-    height: "100vh",
     latitude: 42.2373,
     longitude: -71.5314,
     zoom: 8,
@@ -33,19 +36,35 @@ const MapPage = () => {
   }
 
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={GATSBY_MABOX_API_ACCESS_TOKEN}
-      mapStyle={mapStyle}
-      onViewportChange={setViewport}
-    >
-      <Marker
-        latitude={42.408428}
-        longitude={-71.011993}
-        onClick={setLocation}
-      />
-      {renderPopup()}
-    </ReactMapGL>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          Covid19 PPE donations - Massachusetts
+        </Navbar.Brand>
+        <Navbar.Collapse></Navbar.Collapse>
+        <Button
+          style={{ display: "flex", alignItems: "center" }}
+          href="https://github.com/camilo86/covid19-donate-ma"
+        >
+          <FaGithub style={{ marginRight: 5 }} /> View on GitHub
+        </Button>
+      </Navbar>
+      <ReactMapGL
+        {...viewport}
+        width={width}
+        height={height - 56}
+        mapboxApiAccessToken={GATSBY_MABOX_API_ACCESS_TOKEN}
+        mapStyle={mapStyle}
+        onViewportChange={setViewport}
+      >
+        <Marker
+          latitude={42.408428}
+          longitude={-71.011993}
+          onClick={setLocation}
+        />
+        {renderPopup()}
+      </ReactMapGL>
+    </div>
   )
 }
 
