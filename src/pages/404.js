3d1f8637efb4717b08f1b6d5c9eedaffdd27fe02
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-03-27 21:04:49 -0400

    move map to index and add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,22 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <center>
+        <h1>Page not found</h1>
+        <Link className="btn btn-primary" style={{ color: "white" }} to="/">
+          Return to map
+        </Link>
+      </center>
+    </div>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,102 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+import ReactMapGL, { Popup } from "react-map-gl"
+import { Navbar, Button } from "react-bootstrap"
+import { FaGithub } from "react-icons/fa"
+import useWindowDimensions from "../utils/useWindowDimensions"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
+import Marker from "../components/marker"
+import InfoBox from "../components/InfoBox"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const mapStyle = "mapbox://styles/milomilo86/ck83uyrnc2n4l1imsjgjwmghu"
 
-export default IndexPage
+const MapPage = ({ data }) => {
+  const { GATSBY_MABOX_API_ACCESS_TOKEN } = process.env
+
+  const { width, height } = useWindowDimensions()
+  const [location, setLocation] = useState(null)
+  const [viewport, setViewport] = useState({
+    latitude: 42.2373,
+    longitude: -71.5314,
+    zoom: 8,
+  })
+
+  const renderPopup = () => {
+    if (!location) return null
+
+    return (
+      <Popup
+        anchor="top"
+        longitude={location.geo.longitude}
+        latitude={location.geo.latitude}
+        closeOnClick={false}
+        onClose={() => setLocation(null)}
+      >
+        <InfoBox location={location} />
+      </Popup>
+    )
+  }
+
+  return (
+    <Layout>
+      <Seo title="MA PPE donation map" />
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          Covid19 PPE donations - Massachusetts
+        </Navbar.Brand>
+        <Navbar.Collapse></Navbar.Collapse>
+        <Button
+          style={{ display: "flex", alignItems: "center" }}
+          href="https://github.com/camilo86/covid19-donate-ma"
+        >
+          <FaGithub style={{ marginRight: 5 }} /> View on GitHub
+        </Button>
+      </Navbar>
+      <ReactMapGL
+        {...viewport}
+        width={width}
+        height={height - 56}
+        mapboxApiAccessToken={GATSBY_MABOX_API_ACCESS_TOKEN}
+        mapStyle={mapStyle}
+        onViewportChange={setViewport}
+      >
+        {data.allLocationsYaml.edges.map(({ node: location }) => (
+          <Marker key={location.id} location={location} onClick={setLocation} />
+        ))}
+        {renderPopup()}
+      </ReactMapGL>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allLocationsYaml {
+      edges {
+        node {
+          id
+          name
+          address {
+            line1
+            city
+            state
+            zipcode
+          }
+          geo {
+            longitude
+            latitude
+          }
+          contact {
+            phone
+          }
+          needs
+          sources
+        }
+      }
+    }
+  }
+`
+
+export default MapPage
